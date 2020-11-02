a2a5ca7f6af33e45fc53f5b055fcbc40e1387a9c
Tetiank-a <58983291+Tetiank-a@users.noreply.github.com>
2020-11-02 12:39:23 +0200

    --

--- PRINTER_CENTER/PRINTER_CENTER/Program.cs
+++ PRINTER_CENTER/PRINTER_CENTER/Program.cs
@@ -16,7 +16,7 @@ namespace PRINTER_CENTER
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm()); 
         } 
     }
 }
